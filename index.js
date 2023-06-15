454ebb8a68aea94efd4f9724878f943d9b6af2f6
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-06-15 12:21:36 +0600

    Delete api created

--- index.js
+++ index.js
@@ -84,6 +84,12 @@ async function run() {
       const result = await selectedClassCollection.insertOne(item);
       res.send(result);
     });
+    app.delete("/selected/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedClassCollection.deleteOne(query);
+      res.send(result);
+    });
     app.get("/selected", async (req, res) => {
       const email = req.query.email;
       if (!email) {
